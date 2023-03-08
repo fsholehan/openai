31c5d5ad97783556af4efc11d764855a72a50c33
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2023-03-08 08:25:26 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import { ChatGPTAPI } from "chatgpt";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api", async (req, res) => {
+  const { word } = req.query;
+
+  const api = new ChatGPTAPI({
+    apiKey: process.env.API_OPENAI_KEY,
+    completionParams: {
+      temperature: 0.5,
+      top_p: 0.8,
+    },
+  });
+
+  const response = await api.sendMessage(word);
+  res.json({ ask: word, content: response.text });
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
