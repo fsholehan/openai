51b687bd57ff2071a22974fdd5f628795652e50b
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2023-03-09 07:46:43 +0700

    fix code

--- index.js
+++ index.js
@@ -12,18 +12,23 @@ const app = express();
 app.use(cors());
 
 app.get("/api", async (req, res) => {
-  const { word } = req.query;
+  try {
+    const { word } = req.query;
 
-  const api = new ChatGPTAPI({
-    apiKey: process.env.API_OPENAI_KEY,
-    completionParams: {
-      temperature: 0.5,
-      top_p: 0.8,
-    },
-  });
+    const api = new ChatGPTAPI({
+      apiKey: process.env.API_OPENAI_KEY,
+      completionParams: {
+        temperature: 0.5,
+        top_p: 0.8,
+      },
+    });
 
-  const response = await api.sendMessage(word);
-  res.json({ ask: word, content: response.text });
+    const response = await api.sendMessage(word);
+    res.json({ ask: word, content: response.text });
+  } catch (error) {
+    // console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
